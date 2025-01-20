e4ec469fef13fefaf474266df7f6d472ebf139b8
ayarefaat <ayarefaat_94@yahoo.com>
2025-01-20 08:21:42 +0300

    Auto-generated commit

--- chunk-XVBPYJJ4.js
+++
@@ -1,1 +0,0 @@
-import{ja as i}from"./chunk-6HV3THO2.js";var s=(()=>{class e{transform(r,t){return r.filter(a=>a.title.toLowerCase().includes(t.toLowerCase()))}static{this.\u0275fac=function(t){return new(t||e)}}static{this.\u0275pipe=i({name:"search",type:e,pure:!0,standalone:!0})}}return e})();export{s as SearchPipe};
